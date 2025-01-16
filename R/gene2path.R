3b1fdf9d9ea5d01472f552d7fabb2cb56ea151c6
JoannaZyla <joanna.zyla@polsl.pl>
2025-01-16 13:09:54 +0100

    First function

---
+++ R/gene2path.R
@@ -0,0 +1,30 @@
+#' Function to transform gene level information to pathway level by single-sample approaches.
+#'
+#' Function reduce genes to pathways.
+#'
+#' @param X matrix of data.
+#' @param Y list of pathways
+#' @param method methods for single-sample pathway enrichment analysis.
+#'
+#' @returns Function returns...
+#'
+#' @importFrom
+#' @examples
+#' \dontrun{
+#'
+#'
+#' }
+#'
+#' @seealso \code{\link{gaussian_mixture_vector}}, \code{\link{EM_iter}}
+#'
+#' @export
+gene2path<- function(X, Y, method = "ssGSEA"){
+  # Check part
+  if (!hasArg("X")){
+    stop("No data.")}
+
+
+
+
+
+}
